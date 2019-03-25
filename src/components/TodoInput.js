d68724654d3522079b927ea6191bc166a022a633
csslick <digitone@naver.com>
2019-03-25 22:22:45 +0900

    first commit

---
+++ src/components/TodoInput.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class TodoInput extends Component {
+  render() {
+    return (
+      <h2>TodoInput</h2>
+    );
+  }
+}
+
+export default TodoInput;
---
+++ src/components/TodoItem.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class TodoItem extends Component {
+  render() {
+    return (
+      <>
+        <h2>TodoItem</h2>
+      </>
+    );
+  }
+}
+
+export default TodoItem;
