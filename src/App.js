bb0b39fa710b5f5fc33dd439335ba70b41edeb5d
csslick <digitone@naver.com>
2019-03-25 22:37:53 +0900

    some change1

--- src/App.js
+++ src/App.js
@@ -5,13 +5,14 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import TodoInput from "./components/TodoInput";
 import TodoList from "./components/TodoList";
 
+// some change1
 class App extends Component {
   render() {
     return (
       <>
         <h1>Todo List</h1>
         <div className="container">
-          <div>
+          <div className="row">
             <TodoInput />
             <TodoList />
           </div>
